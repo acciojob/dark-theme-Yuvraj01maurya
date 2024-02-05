db0fd1e646f08e27a03a2f23f414ab33bdd9e81b
Yuvraj Maurya <yuvraj95166@gmail.com>
2024-02-05 23:04:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here
+function swapTheme() {
+  const appDiv = document.getElementById('app');
+  const swapButton = document.getElementById('swap');
+
+  // Toggle classes for the app div
+  appDiv.classList.toggle('day');
+  appDiv.classList.toggle('night');
+
+  // Toggle classes for the swap button
+  swapButton.classList.toggle('button_day');
+  swapButton.classList.toggle('button_night');
+}
 
